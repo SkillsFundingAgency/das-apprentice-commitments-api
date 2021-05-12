910b053a09ed5155d2dff71d7920a8ae7215045b
mld3 <57175069+MarieDale3@users.noreply.github.com>
2021-05-12 08:12:01 +0100

    CS-569 views added with some errors that need fixing, post deployment script added

---
+++ src/SFA.DAS.ApprenticeCommitments.Database/PostDeployment/PostDeployment.sql
@@ -0,0 +1,25 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+/* Developer Role Access */
+
+IF DATABASE_PRINCIPAL_ID('Developer') IS NULL
+BEGIN
+    CREATE ROLE [Developer]
+END
+
+GRANT SELECT ON ApprenticeDashboardView TO Developer
+
+GRANT SELECT ON ApprenticeshipDashboardView TO Developer
+
+GRANT SELECT ON RegistrationDashboardView TO Developer
