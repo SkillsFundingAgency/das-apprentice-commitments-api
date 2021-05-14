2e2b6361f700edf3045cfea5f4f8ff4219dce174
mld3 <57175069+MarieDale3@users.noreply.github.com>
2021-05-14 08:27:58 +0100

    One selection removed

--- src/SFA.DAS.ApprenticeCommitments.Database/Views/RegistrationDashboardView.sql
+++ src/SFA.DAS.ApprenticeCommitments.Database/Views/RegistrationDashboardView.sql
@@ -1,6 +1,6 @@
 ﻿CREATE VIEW dbo.RegistrationDashboardView
 AS
-SELECT        ApprenticeId, ApprenticeshipId, ApprovedOn, Email, CreatedOn, EmployerAccountLegalEntityId, EmployerName, TrainingProviderId, TrainingProviderName, CourseName, CourseLevel, CourseOption, PlannedStartDate, 
+SELECT        ApprenticeId, ApprenticeshipId, ApprovedOn, CreatedOn, EmployerAccountLegalEntityId, EmployerName, TrainingProviderId, TrainingProviderName, CourseName, CourseLevel, CourseOption, PlannedStartDate, 
                          PlannedEndDate, FirstViewedOn, SignUpReminderSentOn
 FROM          dbo.Registration
 GO
