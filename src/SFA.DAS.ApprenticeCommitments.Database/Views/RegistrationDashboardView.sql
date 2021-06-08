f162c0565571a94bc8bc52dab31299132d6317fc
mld3 <57175069+MarieDale3@users.noreply.github.com>
2021-06-08 09:41:18 +0100

    views updates to match tables

--- src/SFA.DAS.ApprenticeCommitments.Database/Views/RegistrationDashboardView.sql
+++ src/SFA.DAS.ApprenticeCommitments.Database/Views/RegistrationDashboardView.sql
@@ -1,6 +1,6 @@
 ﻿CREATE VIEW grafanaReporter.RegistrationDashboardView
 AS
-SELECT        ApprenticeId, ApprenticeshipId, ApprovedOn, CreatedOn, EmployerAccountLegalEntityId, EmployerName, TrainingProviderId, TrainingProviderName, CourseName, CourseLevel, CourseOption, PlannedStartDate, 
+SELECT        ApprenticeId, CommitmentsApprenticeshipId, CommitmentsApprovedOn, CreatedOn, EmployerAccountLegalEntityId, EmployerName, TrainingProviderId, TrainingProviderName, CourseName, CourseLevel, CourseOption, PlannedStartDate, 
                          PlannedEndDate, FirstViewedOn, SignUpReminderSentOn
 FROM          dbo.Registration
 GO
