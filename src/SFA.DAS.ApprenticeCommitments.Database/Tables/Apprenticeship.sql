ce1a1262f98b705756e77138e328ed4b6e9496c8
Ben Arnold <ben@arnoldfamily.me.uk>
2021-07-06 11:10:58 +0100

    Add Apprentice LastViewed to SQL script

--- src/SFA.DAS.ApprenticeCommitments.Database/Tables/Apprenticeship.sql
+++ src/SFA.DAS.ApprenticeCommitments.Database/Tables/Apprenticeship.sql
@@ -3,6 +3,7 @@
 	[Id] BIGINT NOT NULL IDENTITY,
 	[ApprenticeId] UNIQUEIDENTIFIER NOT NULL,
 	[CreatedOn] DATETIME2 NOT NULL DEFAULT current_timestamp, 
+    [LastViewed] DATETIME2 NOT NULL, 
     CONSTRAINT PK_Apprenticeship_Id PRIMARY KEY ([Id]), 
 	CONSTRAINT FK_Apprenticeship_Apprentice_ApprenticeId FOREIGN KEY ([ApprenticeId]) REFERENCES [Apprentice] ([Id])
 )
