52aedb984fa3d48f63b9faa2f6bfbb1e47ff0fca
Ben Arnold <ben@arnoldfamily.me.uk>
2021-07-07 14:20:04 +0100

    🔥 Remove 24 hour logic from ApprenticeChanged event
    It's moved into the Jobs layer

--- src/SFA.DAS.ApprenticeCommitments.Api.AcceptanceTests/WorkflowTests/ChangeNotificationEmail.cs
+++ src/SFA.DAS.ApprenticeCommitments.Api.AcceptanceTests/WorkflowTests/ChangeNotificationEmail.cs
@@ -12,7 +12,7 @@ namespace SFA.DAS.ApprenticeCommitments.Api.AcceptanceTests.WorkflowTests
         public ChangeNotificationEmail() => TimeBetweenActions = TimeSpan.Zero;
 
         [Test]
-        public async Task First_change_within_24_hours_of_creation_sends_email()
+        public async Task First_change_within_24_hours_of_creation_sends_ApprenticeshipChanged_event()
         {
             var (apprenticeship, _) = await CreateApprenticeship(client);
 
@@ -21,11 +21,11 @@ namespace SFA.DAS.ApprenticeCommitments.Api.AcceptanceTests.WorkflowTests
 
             context.Messages.PublishedMessages
                 .Where(x => x.Message is ApprenticeshipChangedEvent)
-                .Should().BeEmpty();
+                .Should().HaveCount(1);
         }
 
         [Test]
-        public async Task First_change_after_24_hours_of_creation_sends_email()
+        public async Task First_change_after_24_hours_of_creation_sends_ApprenticeshipChanged_event()
         {
             var (apprenticeship, _) = await CreateApprenticeship(client);
 
@@ -38,7 +38,7 @@ namespace SFA.DAS.ApprenticeCommitments.Api.AcceptanceTests.WorkflowTests
         }
 
         [Test]
-        public async Task Multiple_change_with_24_hours_sends_only_one_email()
+        public async Task Multiple_change_with_24_hours_sends_only_one_ApprenticeshipChanged_event()
         {
             var (apprenticeship, _) = await CreateApprenticeship(client);
             context.Time.Advance(TimeSpan.FromDays(2));
@@ -49,11 +49,11 @@ namespace SFA.DAS.ApprenticeCommitments.Api.AcceptanceTests.WorkflowTests
 
             context.Messages.PublishedMessages
                 .Where(x => x.Message is ApprenticeshipChangedEvent)
-                .Should().HaveCount(1);
+                .Should().HaveCount(2);
         }
 
         [Test]
-        public async Task Second_change_within_24_hours_after_apprentice_has_viewed_first_sends_email()
+        public async Task Second_change_within_24_hours_after_apprentice_has_viewed_first_sends_ApprenticeshipChanged_event()
         {
             var (apprenticeship, _) = await CreateApprenticeship(client);
 
