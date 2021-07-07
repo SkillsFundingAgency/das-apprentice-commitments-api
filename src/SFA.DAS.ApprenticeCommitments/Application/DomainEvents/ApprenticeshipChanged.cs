155ee0ae9467c2ca15dcbf455a1e1d469463ac2b
Ben Arnold <ben@arnoldfamily.me.uk>
2021-07-07 14:20:04 +0100

    🔥 Remove 24 hour logic from ApprenticeChanged event
    It's moved into the Jobs layer

--- src/SFA.DAS.ApprenticeCommitments/Application/DomainEvents/ApprenticeshipChanged.cs
+++ src/SFA.DAS.ApprenticeCommitments/Application/DomainEvents/ApprenticeshipChanged.cs
@@ -1,10 +1,7 @@
 ﻿using MediatR;
 using NServiceBus;
-using SFA.DAS.ApprenticeCommitments.Configuration;
 using SFA.DAS.ApprenticeCommitments.Data.Models;
 using SFA.DAS.ApprenticeCommitments.Messages.Events;
-using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -21,33 +18,19 @@ namespace SFA.DAS.ApprenticeCommitments.Application.DomainEvents
     internal class ApprenticeshipChangedHandler : INotificationHandler<ApprenticeshipChanged>
     {
         private readonly IMessageSession _messageSession;
-        private readonly TimeSpan _timeToWaitBeforeEmail;
 
-        public ApprenticeshipChangedHandler(IMessageSession messageSession, ApplicationSettings settings)
+        public ApprenticeshipChangedHandler(IMessageSession messageSession)
         {
             _messageSession = messageSession;
-            _timeToWaitBeforeEmail = settings.TimeToWaitBeforeChangeOfApprenticeshipEmail;
         }
 
         public async Task Handle(ApprenticeshipChanged notification, CancellationToken cancellationToken)
         {
-            var ordered = notification.Apprenticeship.CommitmentStatements
-                .OrderBy(x => x.CommitmentsApprovedOn).ToArray();
-
-            var newest = ordered[^1];
-            var previous = ordered[^2];
-
-            var sinceLastApproval = newest.CommitmentsApprovedOn - previous.CommitmentsApprovedOn;
-            var seenPreviousApproval = notification.Apprenticeship.LastViewed > previous.CommitmentsApprovedOn;
-
-            if (sinceLastApproval > _timeToWaitBeforeEmail || seenPreviousApproval)
+            await _messageSession.Publish(new ApprenticeshipChangedEvent
             {
-                await _messageSession.Publish(new ApprenticeshipChangedEvent
-                {
-                    ApprenticeId = notification.Apprenticeship.ApprenticeId,
-                    ApprenticeshipId = notification.Apprenticeship.Id,
-                });
-            }
+                ApprenticeId = notification.Apprenticeship.ApprenticeId,
+                ApprenticeshipId = notification.Apprenticeship.Id,
+            });
         }
     }
 }
