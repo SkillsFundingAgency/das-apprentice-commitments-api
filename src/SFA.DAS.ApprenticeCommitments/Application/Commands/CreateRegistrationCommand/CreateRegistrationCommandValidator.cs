d41d02822e77da1e6a3e69fa361311ad4cda2fd5
Ben Arnold <ben@arnoldfamily.me.uk>
2021-07-10 05:00:17 +0100

    fixup ⚡ Include apprentice name and course name in GET Registration API

--- src/SFA.DAS.ApprenticeCommitments/Application/Commands/CreateRegistrationCommand/CreateRegistrationCommandValidator.cs
+++ src/SFA.DAS.ApprenticeCommitments/Application/Commands/CreateRegistrationCommand/CreateRegistrationCommandValidator.cs
@@ -9,6 +9,8 @@ namespace SFA.DAS.ApprenticeCommitments.Application.Commands.CreateRegistrationC
             RuleFor(model => model.ApprenticeId).Must(id => id != default).WithMessage("The Apprentice Id must be valid");
             RuleFor(model => model.CommitmentsApprenticeshipId).Must(id => id > 0).WithMessage("The ApprenticeshipId must be positive");
             RuleFor(model => model.EmployerAccountLegalEntityId).Must(id => id > 0).WithMessage("The EmployerAccountLegalEntityId must be positive");
+            RuleFor(model => model.FirstName).NotNull().NotEmpty().WithMessage("FirstName is required");
+            RuleFor(model => model.LastName).NotNull().NotEmpty().WithMessage("LastName is required");
             RuleFor(model => model.Email).NotNull().EmailAddress().WithMessage("Email must be a valid email address");
             RuleFor(model => model.EmployerName).NotEmpty().WithMessage("The Employer Name is required");
             RuleFor(model => model.TrainingProviderId).Must(id => id > 0).WithMessage("The TrainingProviderId must be positive");
