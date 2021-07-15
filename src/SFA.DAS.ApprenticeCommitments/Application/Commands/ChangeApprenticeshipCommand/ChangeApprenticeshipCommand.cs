2db5f21027d165aa1028f23e721442f2c371f177
Paul Graham <paul.graham@coprime.co.uk>
2021-07-15 11:28:59 +0100

    fix code smells

--- src/SFA.DAS.ApprenticeCommitments/Application/Commands/ChangeApprenticeshipCommand/ChangeApprenticeshipCommand.cs
+++ src/SFA.DAS.ApprenticeCommitments/Application/Commands/ChangeApprenticeshipCommand/ChangeApprenticeshipCommand.cs
@@ -1,8 +1,6 @@
 ﻿using SFA.DAS.ApprenticeCommitments.Infrastructure.Mediator;
 using System;
 
-#nullable enable
-
 namespace SFA.DAS.ApprenticeCommitments.Application.Commands.ChangeApprenticeshipCommand
 {
     public class ChangeApprenticeshipCommand : IUnitOfWorkCommand
@@ -10,14 +8,14 @@ namespace SFA.DAS.ApprenticeCommitments.Application.Commands.ChangeApprenticeshi
         public long? CommitmentsContinuedApprenticeshipId { get; set; }
         public long CommitmentsApprenticeshipId { get; set; }
         public DateTime CommitmentsApprovedOn { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public string EmployerName { get; set; }
+        public string? EmployerName { get; set; }
         public long EmployerAccountLegalEntityId { get; set; }
         public long TrainingProviderId { get; set; }
-        public string TrainingProviderName { get; set; }
-        public string CourseName { get; set; }
+        public string? TrainingProviderName { get; set; }
+        public string? CourseName { get; set; }
         public int CourseLevel { get; set; }
         public string? CourseOption { get; set; }
         public DateTime PlannedStartDate { get; set; }
