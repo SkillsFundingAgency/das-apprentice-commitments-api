ae6ddc5b4f7a3c6f8867f597633e07ceb6938d43
Ben Arnold <ben@arnoldfamily.me.uk>
2021-07-18 15:52:05 +0100

    Diagnostics

--- src/SFA.DAS.ApprenticeCommitments/Application/Commands/ApprenticeshipShownCommand/UpdateApprenticeshipCommand.cs
+++ src/SFA.DAS.ApprenticeCommitments/Application/Commands/ApprenticeshipShownCommand/UpdateApprenticeshipCommand.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using SFA.DAS.ApprenticeCommitments.Data;
 using SFA.DAS.ApprenticeCommitments.Data.Models;
 using SFA.DAS.ApprenticeCommitments.Infrastructure.Mediator;
@@ -26,14 +28,17 @@ namespace SFA.DAS.ApprenticeCommitments.Application.Commands.ApprenticeshipShown
     public class UpdateApprenticeshipCommandHandler : IRequestHandler<UpdateApprenticeshipCommand>
     {
         private readonly IApprenticeshipContext _apprenticeships;
+        private readonly ILogger<UpdateApprenticeshipCommandHandler> _logger;
 
-        public UpdateApprenticeshipCommandHandler(IApprenticeshipContext apprenticeships)
+        public UpdateApprenticeshipCommandHandler(IApprenticeshipContext apprenticeships, ILogger<UpdateApprenticeshipCommandHandler> logger)
         {
             _apprenticeships = apprenticeships;
+            _logger = logger;
         }
 
         public async Task<Unit> Handle(UpdateApprenticeshipCommand request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation($"Updating {request.ApprenticeId}/{request.ApprenticeshipId} - {JsonConvert.SerializeObject(request.Updates)}");
             var app = await _apprenticeships.GetById(request.ApprenticeId, request.ApprenticeshipId);
             request.Updates.ApplyTo(app);
             return Unit.Value;
