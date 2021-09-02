bfc336eb579eec8f8932e75a14c8608ea4efa462
Ben Arnold <ben@arnoldfamily.me.uk>
2021-09-02 17:33:13 +0100

    🔨 Extract domain event handlers

---
+++ src/SFA.DAS.ApprenticeCommitments/Application/DomainEvents/Handlers/PublishApprenticeshipRegisteredEvent.cs
@@ -0,0 +1,53 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using SFA.DAS.ApprenticeCommitments.Data;
+using SFA.DAS.ApprenticeCommitments.Data.Models;
+using SFA.DAS.ApprenticeCommitments.Messages.Events;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ApprenticeCommitments.Application.DomainEvents
+{
+    internal class PublishApprenticeshipRegisteredEvent
+        : INotificationHandler<RegistrationAdded>
+        , INotificationHandler<RegistrationUpdated>
+    {
+        private readonly IApprenticeContext apprentices;
+        private readonly IMessageSession messageSession;
+        private readonly ILogger<PublishApprenticeshipRegisteredEvent> logger;
+
+        public PublishApprenticeshipRegisteredEvent(IApprenticeContext apprentices, IMessageSession messageSession, ILogger<PublishApprenticeshipRegisteredEvent> logger)
+        {
+            this.apprentices = apprentices;
+            this.messageSession = messageSession;
+            this.logger = logger;
+        }
+
+        public async Task Handle(RegistrationAdded notification, CancellationToken cancellationToken)
+            => await Publish(notification.Registration, nameof(RegistrationAdded));
+
+        public async Task Handle(RegistrationUpdated notification, CancellationToken cancellationToken)
+        {
+            var a = await apprentices.GetByEmail(notification.Registration.Email);
+            if (a.Any()) return;
+
+            await Publish(notification.Registration, nameof(RegistrationUpdated));
+        }
+
+        private async Task Publish(Registration registration, string eventName)
+        {
+            logger.LogInformation(
+                            "{DomainEvent} - Publishing ApprenticeshipRegisteredEvent for Registration {RegistrationId}, CommitmentsApprenticeshipId {CommitmentsApprenticeshipId}",
+                            eventName,
+                            registration.RegistrationId,
+                            registration.CommitmentsApprenticeshipId);
+
+            await messageSession.Publish(new ApprenticeshipRegisteredEvent
+            {
+                RegistrationId = registration.RegistrationId,
+            });
+        }
+    }
+}
--- src/SFA.DAS.ApprenticeCommitments/Application/DomainEvents/RegistrationUpdated.cs
+++ src/SFA.DAS.ApprenticeCommitments/Application/DomainEvents/RegistrationUpdated.cs
@@ -1,12 +1,5 @@
 ﻿using MediatR;
-using Microsoft.Extensions.Logging;
-using NServiceBus;
-using SFA.DAS.ApprenticeCommitments.Data;
 using SFA.DAS.ApprenticeCommitments.Data.Models;
-using SFA.DAS.ApprenticeCommitments.Messages.Events;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace SFA.DAS.ApprenticeCommitments.Application.DomainEvents
 {
@@ -16,46 +9,5 @@ namespace SFA.DAS.ApprenticeCommitments.Application.DomainEvents
 
         public RegistrationUpdated(Registration registration)
             => Registration = registration;
-    }
-
-    internal class RegistrationUpdatedHandler
-        : INotificationHandler<RegistrationAdded>
-        , INotificationHandler<RegistrationUpdated>
-    {
-        private readonly IApprenticeContext apprentices;
-        private readonly IMessageSession messageSession;
-        private readonly ILogger<RegistrationUpdatedHandler> logger;
-
-        public RegistrationUpdatedHandler(IApprenticeContext apprentices, IMessageSession messageSession, ILogger<RegistrationUpdatedHandler> logger)
-        {
-            this.apprentices = apprentices;
-            this.messageSession = messageSession;
-            this.logger = logger;
-        }
-
-        public async Task Handle(RegistrationAdded notification, CancellationToken cancellationToken)
-            => await PublishApprenticeshipRegisteredEvent(notification.Registration, nameof(RegistrationAdded));
-
-        public async Task Handle(RegistrationUpdated notification, CancellationToken cancellationToken)
-        {
-            var a = await apprentices.GetByEmail(notification.Registration.Email);
-            if (a.Any()) return;
-
-            await PublishApprenticeshipRegisteredEvent(notification.Registration, nameof(RegistrationUpdated));
-        }
-
-        private async Task PublishApprenticeshipRegisteredEvent(Registration registration, string eventName)
-        {
-            logger.LogInformation(
-                            "{DomainEvent} - Publishing ApprenticeshipRegisteredEvent for Registration {RegistrationId}, CommitmentsApprenticeshipId {CommitmentsApprenticeshipId}",
-                            eventName,
-                            registration.RegistrationId,
-                            registration.CommitmentsApprenticeshipId);
-
-            await messageSession.Publish(new ApprenticeshipRegisteredEvent
-            {
-                RegistrationId = registration.RegistrationId,
-            });
-        }
     }
 }
