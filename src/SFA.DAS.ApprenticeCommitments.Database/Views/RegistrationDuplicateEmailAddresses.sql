2fbc72f2dd53c8257631d4065047ff21d99e8c5d
Ben Arnold <ben@arnoldfamily.me.uk>
2021-09-28 11:14:06 +0100

    Duplicate email address view

---
+++ src/SFA.DAS.ApprenticeCommitments.Database/Views/RegistrationDuplicateEmailAddresses.sql
@@ -0,0 +1,9 @@
+﻿create view DashboardReporting.RegistrationDuplicateEmailAddresses
+as
+select 
+	count(*)/2 as DuplicateCount
+from 
+	Registration a 
+	left join Registration b on a.Email = b.Email and a.ApprenticeId <> b.ApprenticeId
+where
+	a.email = b.email
