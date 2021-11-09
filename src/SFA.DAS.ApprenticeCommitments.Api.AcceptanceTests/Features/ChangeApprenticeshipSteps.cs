cfcddf2f1441720a4470f4d123acf5332a7e6e5f
Ben Arnold <ben@arnoldfamily.me.uk>
2021-11-09 19:52:19 +0000

    🔨 Introduce Approvals concept for endpoints call by Approvals events

--- src/SFA.DAS.ApprenticeCommitments.Api.AcceptanceTests/Features/ChangeApprenticeshipSteps.cs
+++ src/SFA.DAS.ApprenticeCommitments.Api.AcceptanceTests/Features/ChangeApprenticeshipSteps.cs
@@ -138,7 +138,7 @@ namespace SFA.DAS.ApprenticeCommitments.Api.AcceptanceTests.Features
         [When("the update is posted")]
         public async Task WhenTheUpdateIsPosted()
         {
-            await _context.Api.Put("registrations", _request);
+            await _context.Api.Put("approvals", _request);
         }
 
         [Then("the result should return OK")]
