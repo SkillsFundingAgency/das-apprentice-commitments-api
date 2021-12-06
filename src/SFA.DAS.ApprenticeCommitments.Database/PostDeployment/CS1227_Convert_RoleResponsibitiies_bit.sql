76e554e740357e3f4da572c0394a1deec7bf978f
Ben Arnold <ben@arnoldfamily.me.uk>
2021-12-06 17:01:41 +0000

    🔥 Remove CS-1227 SQL migration

--- src/SFA.DAS.ApprenticeCommitments.Database/PostDeployment/CS1227_Convert_RoleResponsibitiies_bit.sql
+++
@@ -1,8 +0,0 @@
-﻿-- This statement cannot be run on second deployment the code needs to be removed 
-UPDATE Revision SET 
-	RolesAndResponsibilitiesConfirmations = 7
-WHERE RolesAndResponsibilitiesConfirmations IS NOT NULL
-
-UPDATE Revision SET 
-	CreatedOn = CommitmentsApprovedOn
-WHERE CreatedOn IS NULL
