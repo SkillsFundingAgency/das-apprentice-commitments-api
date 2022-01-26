ae97fa32070ca967e8a842826f5599669b4bf7ef
Ben Arnold <ben@arnoldfamily.me.uk>
2022-01-26 10:11:43 +0000

    🔨 Rename timestamp field to AttemptedOn

--- src/SFA.DAS.ApprenticeCommitments.Database/Tables/ApprenticeshipMatchAttempt.sql
+++ src/SFA.DAS.ApprenticeCommitments.Database/Tables/ApprenticeshipMatchAttempt.sql
@@ -4,7 +4,7 @@
     [RegistrationId] uniqueidentifier NOT NULL,
     [ApprenticeId] uniqueidentifier NOT NULL,
     [Status] int NOT NULL,
-    [Timestamp] DATETIME2 NOT NULL DEFAULT GetUtcDate(), 
+    [AttemptedOn] DATETIME2 NOT NULL DEFAULT GetUtcDate(), 
     CONSTRAINT [PK_ApprenticeshipMatchAttempt] PRIMARY KEY ([ApprenticeshipMatchAttemptId]),
     CONSTRAINT [FK_ApprenticeshipMatchAttempt_Registration_RegistrationId] FOREIGN KEY ([RegistrationId]) REFERENCES [Registration] ([RegistrationId]) ON DELETE CASCADE
 )
