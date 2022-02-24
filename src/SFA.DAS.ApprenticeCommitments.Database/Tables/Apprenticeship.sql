8fb4adebf550053e65be2ef100e1fbde750bd56a
Ben Arnold <ben@arnoldfamily.me.uk>
2022-02-24 13:13:12 +0000

    Remove foreign key to Apprentice table

--- src/SFA.DAS.ApprenticeCommitments.Database/Tables/Apprenticeship.sql
+++ src/SFA.DAS.ApprenticeCommitments.Database/Tables/Apprenticeship.sql
@@ -4,7 +4,6 @@
 	[ApprenticeId] UNIQUEIDENTIFIER NOT NULL,
 	[CreatedOn] DATETIME2 NOT NULL DEFAULT current_timestamp, 
     CONSTRAINT PK_Apprenticeship_Id PRIMARY KEY ([Id]), 
-	CONSTRAINT FK_Apprenticeship_Apprentice_ApprenticeId FOREIGN KEY ([ApprenticeId]) REFERENCES [Apprentice] ([Id])
 )
 
 GO
