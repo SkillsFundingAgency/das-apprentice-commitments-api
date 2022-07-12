0365033cc6a10aceb882889171ef5b679ac24039
Paul Graham <paul.graham@coprime.co.uk>
2022-07-12 21:25:28 +0100

    added get revision

---
+++ src/SFA.DAS.ApprenticeCommitments/Application/Queries/ApprenticeshipRevisionQuery/ApprenticeshipRevisionQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using SFA.DAS.ApprenticeCommitments.Data;
+using SFA.DAS.ApprenticeCommitments.DTOs;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ApprenticeCommitments.Application.Queries.ApprenticeshipRevisionQuery
+{
+    public class ApprenticeshipRevisionQueryHandler : IRequestHandler<ApprenticeshipRevisionQuery, ApprenticeshipRevisionDto?>
+    {
+        private readonly IRevisionContext _revisionRepository;
+
+        public ApprenticeshipRevisionQueryHandler(IRevisionContext revisionRepository)
+            => _revisionRepository = revisionRepository;
+
+        public async Task<ApprenticeshipRevisionDto?> Handle(
+            ApprenticeshipRevisionQuery request,
+            CancellationToken cancellationToken)
+        {
+            var entity = await _revisionRepository
+                .GetById(request.ApprenticeId, request.ApprenticeshipId, request.RevisionId);
+
+            return entity?.MapToApprenticeshipRevisionDto();
+        }
+    }
+}
