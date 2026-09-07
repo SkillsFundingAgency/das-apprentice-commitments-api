e989a88e44c8cba3646727b16d87576133928ab6
NWake-CGI <nick.wake@cgi.com>
2026-09-07 17:22:22 +0100

    amend to registration endpoint

--- src/SFA.DAS.ApprenticeCommitments/Data/IRegistrationContext.cs
+++ src/SFA.DAS.ApprenticeCommitments/Data/IRegistrationContext.cs
@@ -35,15 +35,13 @@ namespace SFA.DAS.ApprenticeCommitments.Data
            => Entities.Where(r =>
            r.FirstName == firstName &&
            r.LastName == lastName &&
-           r.DateOfBirth == dateOfBirth &&
-           r.Approval.Course.PlannedStartDate >= DateTime.Today.AddYears(-1) &&
+           r.DateOfBirth == dateOfBirth &&           
            r.Approval.Course.PlannedEndDate >= DateTime.Now)           
            .ToListAsync(cancellationToken);
 
         internal Task<Registration?> FindByEmail(string email, CancellationToken cancellationToken)
             => Entities.Where(r =>
-            r.EmailAddress == email &&
-            r.Approval.Course.PlannedStartDate >= DateTime.Today.AddYears(-1) &&
+            r.EmailAddress == email &&            
             r.Approval.Course.PlannedEndDate >= DateTime.Now)
             .OrderByDescending(r => r.CreatedOn)
             .FirstOrDefaultAsync(cancellationToken);
